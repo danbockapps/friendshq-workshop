7a5dc0fbc88efe1ee2d0921b264d18a7a26b3a95
Dan Bock <danbock@gmail.com>
2018-06-13 09:51:45 -0400

    Refactor to replace template form with reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
@@ -30,7 +30,8 @@ import { EnumToArrayPipe } from './shared/enum-to-array.pipe';
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     FriendsService
--- src/app/people/person-form/person-form.component.ts
+++ src/app/people/person-form/person-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Friend } from '../../shared/friend.model';
 import { Gender } from '../../shared/gender.enum';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-person-form',
@@ -10,22 +11,41 @@ import { Gender } from '../../shared/gender.enum';
 export class PersonFormComponent implements OnInit {
   model: Friend;
   genders = Gender;
+  addNewPersonForm: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.addNewPersonForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      fav: [false],
+      gender: [this.genders]
+    });
+
+    /*
+
+    If I had data I wanted to populate the form with, I could create this model...
+
     this.model = {
       id: null,
       firstName: null,
       lastName: null,
       fav: false,
       gender: null
-    }
-  }
+    };
 
-  ngOnInit() {
+    ... and then do this
+
+    this.addNewPersonForm.setValue(this.model);
+
+    In this case, since we are not pre-populating, we don't need a model object at all.
+    When we send data to the server, it will be the form object, not the model.
+
+    */
   }
 
   onSubmit() {
     console.log("Submitting . . .");
-
   }
 }
