b71c6bb7fcae7412ad45a40445e2b59ab6dc67af
Dan Bock <danbock@gmail.com>
2018-06-13 12:00:08 -0400

    Raju's version of friends spec, with tests passing

--- src/app/shared/friends.service.spec.ts
+++ src/app/shared/friends.service.spec.ts
@@ -1,15 +1,63 @@
-import { TestBed, inject } from '@angular/core/testing';
-
-import { FriendsService } from './friends.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { BASE_URL, FriendsService } from './friends.service';
+import { Gender } from './gender.enum';
+import { Friend } from './friend.model';
 
 describe('FriendsService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let friendsService: FriendsService;
+  let friend;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FriendsService]
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        FriendsService,
+      ],
+    });
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    friendsService = TestBed.get(FriendsService);
+
+    friend = {
+      'id': 1,
+      'firstName': 'Michelle',
+      'lastName': 'Mulroy',
+      'gender': Gender.F,
+      'fav': true
+    };
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  describe('#construction', () => {
+    it('should be created', () => {
+      expect(friendsService).toBeDefined();
     });
   });
 
-  it('should be created', inject([FriendsService], (service: FriendsService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('#getFriends', () => {
+    it('should get all friends', () => {
+      const expectedFriends: Friend[] = [friend];
+
+      friendsService.getFriends()
+        .subscribe(data => {
+          expect(data).toEqual(expectedFriends);
+        });
+
+      const req = httpTestingController.expectOne(`${BASE_URL}/friends`);
+
+      expect(req.request.method).toEqual('GET');
+      req.flush(expectedFriends);
+    });
+  });
 });
--- src/app/shared/friends.service.ts
+++ src/app/shared/friends.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Friend } from "./friend.model";
 
+export const BASE_URL = 'http://localhost:3000';
+
 @Injectable({
   providedIn: 'root'
 })
